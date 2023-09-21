6676ef6b533436b7d465bb3f89ba561a67254656
garret14deras <139816612+garret14deras@users.noreply.github.com>
2023-09-22 01:16:02 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,17 @@
+    data.forEach(item => {
+    });
+function main() {
+    const data = [];
+    return data;
+    for (let i = 0; i < 10; i++) {
+}
+    }
+
+    const data = generateRandomData();
+}
+        console.log("Random Number: " + item);
+
+        data.push(Math.floor(Math.random() * 100) + 1);
+main();
+
+function generateRandomData() {
